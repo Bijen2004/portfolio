a730a7cca6cbad57ffabe99d4160989ba557f0b8
Bijen Shrestha <shrbijen31@gmail.com>
2026-04-05 23:31:52 +0545

    suspence boundary

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,11 +32,11 @@ export default function RootLayout({
         <div className='bg-[#f6a9eb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full  blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#1b1a27]'></div>
         <ThemeContextProvider>
         <ActiveSectionContextProvider>
-        {/* <SuspenseLayout> */}
+        <SuspenseLayout>
           <Header />
           {children}
           <Footer />
-        {/* </SuspenseLayout> */}
+        </SuspenseLayout>
         <Toaster position='top-right'/>
         {/* <Chatbox /> */}
         <ThemeSwitch />
